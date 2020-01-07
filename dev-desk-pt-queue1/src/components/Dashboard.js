902514acec5a4bef0bf4b1e0b3bf2da41e2f5fd1
ConfusionOrb221 <nate1898@gmail.com>
2020-01-06 21:09:57 -0500

    Windows crashed
    so github thinks this is new

--- dev-desk-pt-queue1/src/components/Dashboard.js
+++ dev-desk-pt-queue1/src/components/Dashboard.js
@@ -1,1 +1,42 @@
 //needs to have separate views for the user dashboard and the helper dashboard. You could hash it out as one single component here, or you can delete this file and do a StudentDash.js component and a HelperDashboard.js component
+import React from 'react';
+import propTypes from 'prop-types';
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+    /** Determines which type of user is logged in and displays data based on that */
+    handleView(){
+        switch(this.props.User){
+            case 'Helper':
+                return(
+                    <>
+                    </>
+                )
+            break;
+            case 'User': 
+                return (
+                    <>
+                    </>
+                )
+            break;
+        }
+    }
+
+    render() {
+        return (
+            this.handleView()
+        );
+    }
+}
+
+Dashboard.propTypes = {
+    User: propTypes.string
+};
+
+export default Dashboard;
