a1df053429021aab1b0894676ae1c322c4b6132c
CodingCCarpenter <christineassists@aol.com>
2020-01-08 21:53:31 -0600

    add dependencies for redux, add actions

---
+++ dev-desk-pt-queue1/src/actions/index.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import axiosAuth from '../auth/axiosAuth';
+
+//Actions for login operation 
+
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGIN_RESOLVED = 'LOGIN_RESOLVED';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+
+export const login = credentials => dispatch => {
+    dispatch({ type: LOGIN_START });
+
+    return axios
+        .post('https://dev-desk-queue.herokuapp.com/', credentials)
+        .then(res => {
+            if (res.status === 200){
+                dispatch({
+                    type: LOGIN_SUCCESS,
+                    user: res.data.user,
+                    token: res.data.token,
+                    status: 'success',
+                    message: res.data.message
+                });
+                setTimeout(() => 
+                    dispatch
+                        ({ type: LOGIN_RESOLVED }), 
+                        1500
+                );
+            }
+        })
+       .catch(err => {
+           if(err.response.status === 500 || res.response.status === 404) {
+               dispatch({
+                   type: LOGIN_FAIL,
+                   payload: err.response.data.msg,
+                   status: 'error'
+               });
+           }
+           setTimeout(() => dispatch
+                ({ type: LOGIN_RESOLVED }), 
+                1500
+            );
+       }) ;
+};
