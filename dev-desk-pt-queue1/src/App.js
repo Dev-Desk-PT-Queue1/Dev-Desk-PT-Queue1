46365421543fbc633f2f7820c37350431196afae
ConfusionOrb221 <nate1898@gmail.com>
2020-01-09 17:00:19 -0500

    Added User and Helper

--- dev-desk-pt-queue1/src/App.js
+++ dev-desk-pt-queue1/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import NewTicketForm from './components/NewTicketForm';
 import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <NewTicketForm />
       <Dashboard />
     </div>
   );
